dacd8f1e9e7b11bdd7710bd666d7e55be1aee0fb
SzymonP <szpanek2000@gmail.com>
2025-08-04 16:59:15 +0200

    logical operators

--- index.php
+++ index.php
@@ -23,19 +23,43 @@
 //     echo "You must be 18+ to enter";
 // }
 
-$hours = 50;
-$rate = 15;
-$weekly_pay = null;
+// $hours = 50;
+// $rate = 15;
+// $weekly_pay = null;
 
 
-if ($hours <= 0) {
-    $weekly_pay = 0;
-} elseif ($hours <= 40) {
-    $weekly_pay = $hours * $rate;
+// if ($hours <= 0) {
+//     $weekly_pay = 0;
+// } elseif ($hours <= 40) {
+//     $weekly_pay = $hours * $rate;
+// } else {
+//     $weekly_pay = ($rate * 40) + (($hours - 40) * ($rate * 1.5));
+// }
+
+// echo "Salary: \${$weekly_pay}"
+
+// $temp = 25;
+// $cloudy = true;
+
+// if ($temp >= 0 && $temp <= 30) {
+//     echo "The wether is good <br>";
+// } else {
+//     echo "The wether is bad <br>";
+// }
+
+// if ($cloudy) {
+//     echo "It's cloudy";
+// } else {
+//     echo "It's sunny";
+// }
+
+$age = 18;
+$citizen = true;
+
+if ($age >= 18 && $citizen) {
+    echo "You can vote";
 } else {
-    $weekly_pay = ($rate * 40) + (($hours - 40) * ($rate * 1.5));
+    echo "Sorry you cant vote";
 }
-
-echo "Salary: \${$weekly_pay}"
 
 ?>
