271f08f637128f0c6280057ad52def67ddd8e390
SzymonP <szpanek2000@gmail.com>
2025-08-05 12:17:33 +0200

    associative arrays

--- index.php
+++ index.php
@@ -8,20 +8,47 @@
 </head>
 
 <body>
-
+    <form action="index.php" method="post">
+        <label>Enter a country</label>
+        <input type="text" name="country">
+        <input type="submit" value="search">
+    </form>
 </body>
 
 </html>
 <?php
-$foods = array("apple", "bannana", "orange");
+// $foods = array("apple", "bannana", "orange");
 
-// array_push($foods, "pineapple", "kiwi");
-// array_pop($foods);
-// array_shift($foods);
-$foods = array_reverse($foods);
-echo count($foods) . "<br>";
+// // array_push($foods, "pineapple", "kiwi");
+// // array_pop($foods);
+// // array_shift($foods);
+// $foods = array_reverse($foods);
+// echo count($foods) . "<br>";
 
-foreach ($foods as $food) {
-    echo $food . "<br>";
-}
+// foreach ($foods as $food) {
+//     echo $food . "<br>";
+// }
+
+
+// associative arrays
+
+$capitals = array(
+    "Poland" => "Warsaw",
+    "USA" => "Washington D.C",
+    "Japan" => "Kyoto",
+    "Germany" => "Berlin"
+);
+
+// $capitals["Poland"] = "Cracow";
+// $capitals["China"] = "Beijing";
+
+
+// foreach ($capitals as $key => $value) {
+//     echo $value . " = " .  $key . "<br>";
+// }
+
+
+$capital = $capitals[$_POST["country"]];
+
+echo $capital;
 ?>
