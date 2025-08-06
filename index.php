46ac4056cd74e82683c985a6ba715f3575968068
SzymonP <szpanek2000@gmail.com>
2025-08-06 11:46:52 +0200

    radio buttons

--- index.php
+++ index.php
@@ -9,30 +9,37 @@
 
 <body>
     <form action="index.php" method="post">
-        <label>username</label>
-        <input type="text" name="username">
-        <br>
-        <label>password</label>
-        <input type="text" name="password"> <br>
-        <input type="submit" value="Log In" name="login">
+        <input type="radio" value="Visa" name="credit_card">
+        Visa <br>
+        <input type="radio" value="MasterCard" name="credit_card">
+        MasterCard <br>
+        <input type="radio" value="AmericanExpress" name="credit_card">
+        AmericanExpress <br>
+        <input type="submit" name="confirm" value="confirm">
     </form>
 </body>
 
 </html>
 <?php
-foreach ($_POST as $key => $value) {
-    echo "{$key} = {$value} <br>";
+if (isset($_POST["confirm"])) {
+    $credit_card = null;
+
+    if (isset($_POST["credit_card"])) {
+        $credit_card = $_POST["credit_card"];
+    }
+    switch ($credit_card) {
+        case "Visa":
+            echo "You selected Visa";
+            break;
+        case "MasterCard":
+            echo "You selected MasterCard";
+            break;
+        case "AmericanExpress":
+            echo "You selected AmericanExpress";
+            break;
+        default:
+            echo "Please make a selection";
+    }
 }
 
-// if (isset($_POST["login"])) {
-//     $username = $_POST["username"];
-//     $password = $_POST["password"];
-//     if (empty($username)) {
-//         echo "Username is missing";
-//     } elseif (empty($password)) {
-//         echo "Password is missing";
-//     } else {
-//         echo "Welcome {$username}";
-//     }
-// }
 ?>
