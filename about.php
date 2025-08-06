a9b8dad3307993897e40bf0998d1debbade1aba5
SzymonP <szpanek2000@gmail.com>
2025-08-06 15:36:19 +0200

    include function

--- about.php
+++ about.php
@@ -1,22 +1,20 @@
 <?php
-include("header.html");
+include("header.html")
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
 
 <head>
-    <meta charset=" UTF-8">
+    <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
 
 <body>
-    This is my Home page <br>
+    This is my ABOUT page <br>
     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Natus modi in aliquid at temporibus eius
+
 </body>
 
 </html>
-<?php
-include("footer.html");
-?>
