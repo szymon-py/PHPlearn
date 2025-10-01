b7378a06836ed87580c5ad52233442913cda4e53
SzymonP <szpanek2000@gmail.com>
2025-10-01 11:16:27 +0200

    Simple calculator in PHP

--- index.php
+++ index.php
@@ -8,23 +8,45 @@
 </head>
 
 <body>
-    <form action="index.php" method="post">
-        <label>radius in cm: </label>
-        <input type="text" name="radius">
-        <input type="submit" value="calculate">
-    </form><br>
+    <form action="" method="get">
+        <p>Enter X and Y</p>
+        <input type="number" name="x" placeholder="X">
+        <input type="number" name="y" placeholder="Y">
+        <br>
+        <input type="radio" name="operation" value="add"> Add
+        <input type="radio" name="operation" value="subtract"> Subtract
+        <input type="radio" name="operation" value="multiply"> Multiply
+        <input type="radio" name="operation" value="divide"> Divide
+        <br>
+        <button type="submit">Submit</button>
 </body>
 
 </html>
+
 <?php
-$radius = $_POST["radius"];
-$circumference = null;
 
-$circumference = round((2 * pi() * $radius), 2);
-$area = round(pi() * pow($radius, 2), 2);
-$volue = round(4 / 3 * pi() * pow($radius, 3), 2);
 
-echo "Circumference is: {$circumference}cm <br>";
-echo "Area is: {$area}cm^2 <br>";
-echo "Volue is: {$volue}cm^3 <br>";
-?>
+if (isset($_GET['x']) && isset($_GET['y']) && isset($_GET['operation'])) {
+    $x = (int) $_GET['x'];
+    $y = (int) $_GET['y'];
+    $op = $_GET['operation'];
+
+    switch ($op) {
+        case 'add':
+            $result = $x + $y;
+            break;
+        case 'subtract':
+            $result = $x - $y;
+            break;
+        case 'multiply':
+            $result = $x * $y;
+            break;
+        case 'divide':
+            $result = $y != 0 ? $x / $y : "Division by zero!";
+            break;
+        default:
+            $result = "Unknown operation";
+    }
+
+    echo "<p> Result: $result</p>";
+}
